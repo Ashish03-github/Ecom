ebda42554b64cbc2e0e27d62cff0f371373f3135
Ashish <ashishyadav031001@gmail.com>
2025-12-31 16:12:23 +0530

    setup: basic project setup

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,22 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        alias: {
+          '@': './src',
+          '@/features/': './src/features',
+          '@/common/': './src/common',
+          '@/constants/': './src/constants',
+          '@/navigation/': './src/navigation',
+          '@/services/': './src/services',
+          '@/store/': './src/store',
+          '@/theme/': './src/theme',
+          '@/utils/': './src/utills',
+        },
+      },
+    ],
+  ],
 };
---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+const App = () => {
+  return (
+    <View>
+      <Text>App is working</Text>
+    </View>
+  );
+};
+
+export default App;
